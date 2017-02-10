9e076496a77f0d7893e64af5a368259532f5fd94
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2017-02-10 10:10:55 -0600

    Añade el escenario DESCONOCIDO

--- SQL/Seeds/Seed_CUP_ElminarSaldosMenoresInv_Escenarios.sql
+++ SQL/Seeds/Seed_CUP_ElminarSaldosMenoresInv_Escenarios.sql
@@ -13,3 +13,31 @@ VALUES
   63527,
   GETDATE()
 )
+
+SET IDENTITY_INSERT dbo.CUP_EliminarSaldosMenoresInv_Escenarios ON;  
+
+INSERT INTO
+  CUP_EliminarSaldosMenoresInv_Escenarios 
+(
+  ID,
+  Descripcion,
+  Empleado,
+  FechaAlta
+)
+VALUES
+(
+  0,
+  'DESCONOCIDO',
+  63527,
+  GETDATE()
+)
+
+SET IDENTITY_INSERT dbo.CUP_EliminarSaldosMenoresInv_Escenarios OFF;  
+
+SELECT
+  Id,
+  Descripcion,
+  Empleado,
+  FechaAlta
+FROM 
+  CUP_EliminarSaldosMenoresInv_Escenarios
