475040f348e5464fa36431b58c9e613d0e28c831
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2017-02-14 11:39:42 -0600

    Mejora el procedimiento encargado de devolver el resultado de los procesos de ajuste

--- SQL/Stored Procedures/CUP_SPQ_EliminarSaldosMenoresInv_ResultadoDelProceso.sql
+++ SQL/Stored Procedures/CUP_SPQ_EliminarSaldosMenoresInv_ResultadoDelProceso.sql
@@ -19,6 +19,8 @@ GO
 
   Description: Devuelve el resultado de un proceso
   de eliminacion de saldos menores inv.
+
+  EXAMPLE: EXEC CUP_SPQ_EliminarSaldosMenoresInv_ResultadoDelProceso 1
  
 ============================================= */
 
@@ -31,24 +33,90 @@ AS BEGIN
     proceso.Usuario,
     proceso_ajustes.Modulo,
     proceso_ajustes.ModuloId,
-    proceso_ajustes.Escenario,
+    Sucursal = suc.CUP_Alias,
+    i.Mov,
+    i.Movid,
     i.Almacen,
     i.Estatus,
-    ab.Accion,
-    ab.Base,
-    ab.GenerarMov,
-    ab.Usuario,
-    FechaRegistro = CONVERT( VARCHAR(MAX), ab.FechaRegistro, 121),
-    ab.Ok,
-    ab.OkRef,
-    MensajeDesc =  m.Descripcion
+    FechaAfectacion = CONVERT( VARCHAR(MAX), ab.FechaRegistro, 121),
+    PartidasAfectadas = ISNULL(totales_detalle.Partidas,0),
+    KgsTotales_Detalle = ISNULL(totales_detalle.KgsTotales,0),
+    SeriesLoteAfectados = ISNULL(totales_sl.SeriesLote,0),
+    KgsTotales_SerieLote = ISNULL(totales_sl.KgsTotales,0),
+    CostoTotal = ISNULL(totales_detalle.CostoTotal,0),
+    Cant_Mas_Pequeña_Detalle = ISNULL(totales_detalle.CantMasPequeña,0),
+    Cant_Mas_Pequeña_SerieLote = ISNULL(totales_detalle.CantMasGrande,0),
+    Cant_Mas_Grande_Detalle = ISNULL(totales_sl.CantMasPequeña,0),
+    Cant_Mas_Grande_SerieLote = ISNULL(totales_sl.CantMasGrande,0),
+    Error = ISNULL(CAST(ab.Ok AS VARCHAR(MAX)),''),
+    ErrorRef = ISNULL(ab.OkRef,''),
+    ErrorDescripcion = ISNULL(m.Descripcion,'')
   FROM
     CUP_EliminarSaldosMenoresInv proceso
   JOIN CUP_EliminarSaldosMenoresInv_AjustesGenerados proceso_ajustes ON proceso_ajustes.ID = proceso.ID
   JOIN inv i ON i.ID = proceso_ajustes.ModuloID
+  JOIN Sucursal suc ON suc.Sucursal = i.Sucursal
   LEFT JOIN AfectarBitacora ab ON ab.Modulo = proceso_ajustes.Modulo
                               AND ab.ModuloID = proceso_ajustes.ModuloID
   LEFT JOIN MensajeLista m ON m.Mensaje = ab.OK
+  OUTER APPLY(
+               SELECT 
+                 Partidas = COUNT(d.RenglonID),
+                 CantMasGrande = MAX(ISNULL(d.Cantidad,0)),
+                 CantMasPequeña= MIN(ISNULL(d.Cantidad,0)),
+                 CostoTotal = SUM(ISNULL(d.Cantidad,0) * ISNULL(d.Costo,0)),
+                 KgsTotales = SUM
+                              (
+                                  ISNULL(d.Cantidad,0)
+                                * CASE ISNULL(a.Unidad,'')
+                                    WHEN 'KGS'
+                                      THEN 1
+                                    ELSE
+                                      ISNULL(a.Peso,1)
+                                  END
+                              )
+               FROM 
+                 InvD d
+               JOIN art a ON a.Articulo = d.Articulo
+               WHERE 
+                d.ID = i.ID
+             ) totales_detalle
+  OUTER APPLY(
+               SELECT 
+                 SeriesLote = COUNT(slm.SerieLote),
+                 CantMasGrande = MAX(ISNULL(slm.Cantidad,0) * calc.Factor),
+                 CantMasPequeña= MIN(ISNULL(slm.Cantidad,0) * calc.Factor) ,
+                 KgsTotales = SUM
+                              (
+                                ISNULL(slm.Cantidad,0) 
+                              * calc.Factor
+                              * CASE ISNULL(a.Unidad,'')
+                                  WHEN 'KGS'
+                                    THEN 1 
+                                  ELSE
+                                    ISNULL(a.Peso,1) 
+                                END
+                              )
+               FROM 
+                 SerieLoteMov slm
+               JOIN InvD d ON d.Id  = slm.ID 
+                          AnD d.RenglonID = slm.RenglonID
+                          AND d.Articulo  = slm.Articulo
+                          AND ISNULL(d.SubCuenta,'') = ISNULL(slm.SubCuenta,'')
+               JOIN art a ON a.Articulo = slm.Articulo
+               -- calculados
+               CROSS APPLY( SELECT 
+                              Factor = CASE
+                                         WHEN ISNULL(d.Cantidad,0) < 1
+                                           THEN -1 
+                                         ELSE 
+                                           1
+                                       END 
+                          ) calc
+               WHERE 
+                 slm.Modulo = 'INV'
+               AND slm.ID   = i.ID   
+             ) totales_sl
   WHERE 
     proceso.Id = @ID
   AND proceso_ajustes.Modulo = 'INV'
